ed130df82eda8a2ebc9f14ccd112121c34448655
Archit <archittyagi08@gmail.com>
2023-10-21 19:06:31 +0530

    add logo and some images

--- src/Components/Homepage/CategoryList/CategoryList.js
+++ src/Components/Homepage/CategoryList/CategoryList.js
@@ -8,37 +8,41 @@ import Image2 from '../../../Assets/Images/image2.jpg'
 import Image3 from '../../../Assets/Images/image3.jpg'
 import Image4 from '../../../Assets/Images/image4.jpg'
 import Image5 from '../../../Assets/Images/image1.jpg'
+import Image6 from '../../../Assets/Images/image6.jpg'
+import Image7 from '../../../Assets/Images/image7.jpg'
+
+
 
 const list = [
     {
         id : 1,
-        name :  'Category 1',
+        name :  'Construction',
         icon : Image1
     },
     {
         id : 2,
-        name :  'Category 2',
+        name :  'Services',
         icon : Image2
     },
     {
         id : 3,
-        name :  'Category 3',
+        name :  'Customer Service',
         icon : Image3
     },
     {
         id : 4,
-        name :  'Category 4',
+        name :  'Hospital',
         icon : Image4
     },
     {
         id : 5,
         name :  'Category 5',
-        icon : Image5
+        icon : Image7
     },
     {
         id : 6,
-        name :  'Category 6',
-        icon : Image1
+        name :  'Home Services',
+        icon : Image6
     },
 ]
 
